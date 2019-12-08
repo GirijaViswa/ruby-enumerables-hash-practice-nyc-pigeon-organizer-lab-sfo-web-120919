98ecade6a4ff90d97e7d858857ee97b7d90795cb
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-08 03:43:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,5 +4,10 @@ end
 
 def pigeon_name(data)
 new_arr = []
-
+data.each do |key1,value1|
+  value1.each do |key2,value2|
+    value2.each {|ele| new_arr << ele}
+  end
+end
+new_arr = new_arr.uniq
 end
