c54d780b17e979471373fb5e8faf39656375267b
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-08 04:10:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
   name = pigeon_name (data) 
-  color = pigeon_color (data[:co])
+  color = pigeon_color (name,data[:color])
+  pp color
 end
 
 def pigeon_name(data)
@@ -13,4 +14,19 @@ end
 new_arr = new_arr.uniq
 end
 
-def pigeon_color ()
+def pigeon_color (name,data)
+  new_hash = {}
+  
+  name.each do |ele|
+    new_arr = []
+    data.each do |col,arr|
+      arr.each do |pg_name|
+        if pg_name == ele
+          new_arr << col
+        end  
+      end
+    end
+    new_hash[ele] = new_arr
+  end
+  new_hash
+end
